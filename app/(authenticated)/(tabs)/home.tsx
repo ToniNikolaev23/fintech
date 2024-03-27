1fc57978a62eab1cf07bbf0b7ca9223ae1241b62
Toni Stoyanov <toni@1forfit.com>
2024-03-27 15:23:34 +0200

    Create reusable Button and create Dropdown context menu

--- app/(authenticated)/(tabs)/home.tsx
+++ app/(authenticated)/(tabs)/home.tsx
@@ -1,11 +1,53 @@
-import { Text, View } from "react-native";
+import { Button, ScrollView, StyleSheet, Text, View } from "react-native";
+import Colors from "@/constants/Colors";
+import RoundButton from "@/components/RoundButton";
+import Dropdown from "@/components/Dropdown";
 
 const Page = () => {
+  const balance = 5250;
+
+  const onAddMoney = () => {};
   return (
-    <View>
-      <Text>Page</Text>
-    </View>
+    <ScrollView style={{ backgroundColor: Colors.background }}>
+      <View style={styles.account}>
+        <View style={styles.row}>
+          <Text style={styles.balance}>{balance}</Text>
+          <Text style={styles.currency}>$</Text>
+        </View>
+      </View>
+      <View style={styles.actionRow}>
+        <RoundButton icon={"add"} text="Add money" onPress={onAddMoney} />
+        <RoundButton icon={"refresh"} text="Exchange" />
+        <RoundButton icon={"list"} text="Details" />
+        <Dropdown />
+      </View>
+    </ScrollView>
   );
 };
 
+const styles = StyleSheet.create({
+  account: {
+    margin: 80,
+    alignItems: "center",
+  },
+  row: {
+    flexDirection: "row",
+    alignItems: "flex-end",
+    justifyContent: "center",
+    gap: 10,
+  },
+  balance: {
+    fontSize: 50,
+    fontWeight: "bold",
+  },
+  currency: {
+    fontSize: 20,
+    fontWeight: "500",
+  },
+  actionRow: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    padding: 20,
+  },
+});
 export default Page;
